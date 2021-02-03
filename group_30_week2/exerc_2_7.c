233bbf62461f5f6d6445dfe39ab10fb6431ee664
Christian_O'Neill <gusonech@student.gu.se>
2021-02-03 12:56:42 +0100

    exerc 2_7-8

--- group_30_week2/exerc_2_7.c
+++ group_30_week2/exerc_2_7.c
@@ -11,54 +11,96 @@
  Demonstration code: [<Ass code 1-4> <abc>]
  ====================================== */
 
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
- 
- #define MAX 10 // Max length for personnummer
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void readPersnr(char *person) {
-    char month_nums[] = {person[2], person[3]};
-    char day_nums[] = {person[4], person[5]};
-    if ((atoi(month_nums) < 1 || atoi(month_nums) > 12) || (atoi(day_nums) < 1 || atoi(day_nums) > 31)) {
-        printf("Invalid personnummer.\n");
-        return;
-    } else {
-        controlDigit(person);
-    }
+#define MAX 10 // Max length for personnummer
+
+int charToInt(char c)
+{
+    // Substract '0' from entered char to get digit
+    int num = 0;
+    num = c -'0';
+
+    return num;
 }
 
-int controlDigit(const char *persnr) {
-    int sum = 0;
-    for (int i = 0; i < sizeof(persnr); i++) {
-        int cur = atoi(persnr[i]);
-        if (i % 2 == 0) {
+int controlDigit(const char *persnr)
+{
+    int sum = 0; // Init sum as 0
+    for (int i = 0; i < MAX; i++)
+    {
+        // Convert current 
+        int cur = charToInt(persnr[i]);
+        // Multiply every other number by 2
+        if (i % 2 == 0)
+        {
             cur = cur * 2;
-            if (cur > 9) {
-                int temp = cur % 10; // 13 % 10 = 3
-                cur = (cur / 10) + temp;
+            // If the multiplied number is >= 10, split the number and add its parts
+            if (cur > 9)
+            {
+                // e.g (13 / 10) + (13 % 10) = 1 + 3 = 4
+                cur = (cur / 10) + (cur % 10);
             }
         }
+        // Add the current calculated value to the sum
         sum += cur;
     }
-    printf("%d", sum);
+    // If the sum is divisible by 10 then it is a valid personnummer
+    if (sum % 10 == 0)
+    {
+        return 1;
+    }
+    return 0;
 }
 
-int main(void) {
+int readPersnr(char *person)
+{
+    // Multiply by 10 to convert to double digit months / dates    e.g. 2*10+7 = 27
+    int month_nums = (charToInt(person[2])* 10) + charToInt(person[3]);
+    int day_nums = (charToInt(person[4])* 10) + charToInt(person[5]);
+    // Check if month and day is valid
+    if ((month_nums < 1 || month_nums > 12) || (day_nums < 1 || day_nums > 31))
+    {
+        printf("Invalid personnummer.\n");
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main(void)
+{
     char s_personnummer[MAX];
-    while(1) {
+    while (1)
+    {
         // Prompt for input and assign it to s_personnummer
         printf("Please enter your personnummer: \n>> ");
         gets(s_personnummer);
         // Compare if s_personnummer is invalid or quit command
-        if (s_personnummer[0] == 'q' || s_personnummer[0] == 'Q') {
+        if (s_personnummer[0] == 'q' || s_personnummer[0] == 'Q')
+        {
             printf("Goodbye.\n");
             exit(0);
-        } else if (strlen(s_personnummer) != 10) {
+        }
+        else if (strlen(s_personnummer) != 10)
+        {
             printf("Please enter a valid personnummer.\n");
-        // If not, assign it to the s_personnummer
-        } else {
-            readPersnr(s_personnummer);
+            // If not, assign it to the s_personnummer
+        }
+        else if (readPersnr(s_personnummer))
+        {
+            if (controlDigit(s_personnummer))
+            {
+                printf("This is a valid personnummer.\n");
+            }
+            else
+            {
+                printf("This is not a valid personnummer.\n");
+            }
         }
     }
     return 0;
---
+++ group_30_week2/exerc_2_8.c
@@ -0,0 +1,190 @@
+/* ====================================
+ File name: exerc_2_8.c
+ Date: 2021-02-02
+ Group 30
+ Members that contribute to the solutions
+ Christian O'Neill
+ Hugo Hempel
+ Hjalmar Thunberg
+ Member not present at demonstration time:
+ Yyyyyy  yyyy
+ Demonstration code: [<Ass code 1-4> <abc>]
+ ====================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define MAX_COINS 13
+
+const int HUMAN = 0;
+const int COMPUTER = 1;
+
+/* ------------- IO --------------- */
+
+int human_choice(int pile);
+
+void write_winner(int player);
+
+int play_again();
+
+/* ---------------- Logic ----------------*/
+
+int computer_choice(int pile);
+
+int toggle(int player);
+
+/* --------------------- Utilities -------------*/
+void clear_stdin();
+
+/***************************************************
+ *
+ *    MAIN
+ *
+ ***************************************************/
+int main()
+{
+  int pile,    /* This is how many coins we have */
+      player,  /* Who is playing? */
+      n_coins; /* Number of coins taken */
+  
+  pile = MAX_COINS; /* Set start values (= init) */
+  player = HUMAN;
+
+  srand(time(0)); /* Setup random */
+  printf("\n\n-----------------------------------------\n  ~~~~ Welcome to NIM by Group 30 ~~~~~\n-----------------------------------------\n\n");
+
+  /* 
+   *  Program main loop 
+   */
+  while (true)
+  {
+    // prints the amount of coins in pile
+    printf("There are %d coins in the pile\n\n", pile);
+    //checks if player is == HUMAN, if it is call the human_choice function else call the computer_choice function
+    if (player == HUMAN)
+    {
+      n_coins = human_choice(pile);
+      printf("You took %d coins\n", n_coins);
+    }
+    else
+    {
+      n_coins = computer_choice(pile);
+      printf("Computer took %d coins\n", n_coins);
+    }
+    //updates the value of pile
+    pile -= n_coins;
+    // checks if pile is <=1 and prints the winner if it is.
+    if (pile <= 1)
+    {
+      write_winner(player);
+      //calls the play_again function and either continues the game or breaks out of the loop.
+      if (play_again()) {
+        pile = MAX_COINS;
+        player = HUMAN;
+        continue;
+      } else {
+        break;
+      }
+    }
+    else
+    {
+      player = toggle(player);
+    }
+  }
+  /*
+   * end main loop
+   */
+  printf("\n\n-----------------------------------------\n   ~~~~~   Thanks for playing   ~~~~~ \n-----------------------------------------\n\n");
+
+  return 0;
+}
+
+/******************************************************
+ *
+ *  DEFINITIONS
+ * 
+ ******************************************************/
+
+void clear_stdin()
+{
+  while (getchar() != '\n')
+  {
+    ;
+  }
+}
+
+// Convert chars to int values
+int charToInt(char c)
+{
+  return (int)c - '0';
+}
+
+int human_choice(int pile)
+{
+  char choice[2];
+  while (1)
+  {
+    // Get user input
+    printf("Enter your choice (1-3):\n>> ");
+    fgets(choice, 2, stdin);
+    // Convert the number char to an int
+    int c = charToInt(choice[0]);
+    clear_stdin();
+    // Verify it is a valid input
+    if (c < 1 || c > 3 || c > pile-1)
+    {
+      printf("The value must be between 1-3 and less than the pile.\n");
+      continue;
+    }
+    else
+    {
+      return c;
+    }
+  }
+}
+// calculates and returns the computers choice
+int computer_choice(int pile)
+{
+  //returns 1,2 or 3 unless pile is <=4 in which case it will return the best choice, eg. pile=4 return =3 
+  int compChoice = rand() % 3 + 1; // Between 1-3
+  return pile <= 4 ? pile - 1 : compChoice;
+}
+
+//prints the winner of the game
+void write_winner(int player)
+{
+  if (player == HUMAN)
+  {
+    printf("The human wins!\n");
+  }
+  else
+  {
+    printf("The computer wins!\n");
+  }
+}
+
+int play_again()
+{
+  char choice[2];
+  while (1)
+  {
+    printf("Do you wish to play again?\nWrite n/N to exit, or any key to play again.\n");
+    fgets(choice, 2, stdin);
+    clear_stdin();
+    if (choice[0] == 'n' || choice[0] == 'N')
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
+  }
+}
+
+int toggle(int player)
+{
+  return player == HUMAN ? 1 : 0;
+}
